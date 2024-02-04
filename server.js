c21dc7dbd05d5acdf8a2905129b7accac3e539de
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-02-04 01:30:11 -0500

    Changed back required cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Budget API
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 
 app.use("/", express.static('public'));
 
@@ -37,7 +39,7 @@ app.listen(port, () => {
 // const app = express();
 // const port = 3000;
 
-// app.use(cors());
+
 
 
 
